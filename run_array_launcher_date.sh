9fccc150ccdafbb3436f774add264fdd56424f9c
Dongah Kim <donga0223@gmail.com>
2026-07-07 17:35:29 -0400

    launcher

--- run_array_launcher_date.sh
+++ run_array_launcher_date.sh
@@ -27,19 +27,20 @@ mkdir -p launcher_jobs logs
 METHOD="${METHOD:?METHOD is required}"
 DATE_LIST="${DATE_LIST:?DATE_LIST is required}"
 K_LIST="${K_LIST:-7 9 15 21 23 31 45 61}"
-
-K_LIST_FOR_PY=$(echo "$K_LIST" | tr ' ' ',')
+K_VALUES=$(printf '%s' "$K_LIST" | tr ',' ' ')
 
 export LAUNCHER_JOB_FILE=launcher_jobs/launcher_jobs_${METHOD}_${SLURM_JOB_ID}.txt
 rm -f "$LAUNCHER_JOB_FILE"
 
 for DATE in $DATE_LIST; do
-  echo "python -u code/forecasting/run_forecast.py --forecast_date $DATE --method_name $METHOD --k_list $K_LIST_FOR_PY --n_workers 1" >> "$LAUNCHER_JOB_FILE"
+  for K in $K_VALUES; do
+    echo "python -u code/forecasting/run_forecast.py --forecast_date $DATE --method_name $METHOD --k_list $K --n_workers 1" >> "$LAUNCHER_JOB_FILE"
+  done
 done
 
 echo "METHOD=$METHOD"
 echo "K_LIST=$K_LIST"
-echo "K_LIST_FOR_PY=$K_LIST_FOR_PY"
+echo "K_VALUES=$K_VALUES"
 echo "DATE_LIST=$DATE_LIST"
 echo "LAUNCHER_JOB_FILE=$LAUNCHER_JOB_FILE"
 
