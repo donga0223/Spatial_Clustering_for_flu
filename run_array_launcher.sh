1902f4464aa523317ab33740eeb66eac35d41fd2
Dongah Kim <donga0223@gmail.com>
2026-07-07 13:34:11 -0500

    update launcher script

--- run_array_launcher.sh
+++ run_array_launcher.sh
@@ -2,7 +2,7 @@
 #SBATCH -J gbqr_all
 #SBATCH -p small
 #SBATCH -N 1
-#SBATCH --ntasks=21
+#SBATCH --ntasks=10
 #SBATCH --cpus-per-task=1
 #SBATCH -t 12:00:00
 #SBATCH -o logs/slurm_%j.out
@@ -27,13 +27,13 @@ mkdir -p launcher_jobs
 export LAUNCHER_JOB_FILE=launcher_jobs/launcher_jobs_${METHOD}_${DATE}.txt
 rm -f "$LAUNCHER_JOB_FILE"
 
-#for K in $(seq 5 2 45); do
-#  echo "python -u code/forecasting/run_forecast.py --forecast_date $DATE --method_name $METHOD --k_min $K --k_max $K --n_workers 1" >> "$LAUNCHER_JOB_FILE"
-#done
-
-for K in $(seq 2 22); do
-  echo "python -u code/forecasting/run_hsa_forecast.py --forecast_date $DATE --method_name $METHOD --k_min $K --k_max $K --n_workers 1" >> "$LAUNCHER_JOB_FILE"
+for K in $(seq 47 2 65); do
+  echo "python -u code/forecasting/run_forecast.py --forecast_date $DATE --method_name $METHOD --k_min $K --k_max $K --n_workers 1" >> "$LAUNCHER_JOB_FILE"
 done
+
+#for K in $(seq 2 22); do
+#  echo "python -u code/forecasting/run_hsa_forecast.py --forecast_date $DATE --method_name $METHOD --k_min $K --k_max $K --n_workers 1" >> "$LAUNCHER_JOB_FILE"
+#done
 
 echo "LAUNCHER_JOB_FILE=$LAUNCHER_JOB_FILE"
 echo "LAUNCHER_DIR=$LAUNCHER_DIR"
